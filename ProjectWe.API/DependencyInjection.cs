dba6a935cab408a849385da8571416953b4b01ae
mirzaceligija <mirza.celigija@outlook.com>
2023-03-30 00:53:34 +0200

    Extracted DI in a sperate class

---
+++ ProjectWe.API/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using ProjectWe.API.Filters;
+
+namespace ProjectWe.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddControllers(config =>
+            {
+                config.Filters.Add<ErrorFilter>();
+            });
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            //TODO: Add Automapper
+            //builder.Services.AddAutoMapper(typeof(IUsersService));
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Administrator",
+                authBuilder =>
+                {
+                    authBuilder.RequireRole("Administrator");
+                });
+                options.AddPolicy("Manager",
+                    authBuilder =>
+                    {
+                        authBuilder.RequireRole("Manager");
+                    });
+            });
+
+            return services;
+        }
+    }
+}
--- ProjectWe.API/Program.cs
+++ ProjectWe.API/Program.cs
@@ -1,41 +1,22 @@
-using ProjectWe.API.Filters;
+using ProjectWe.API;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers(config =>
 {
-    config.Filters.Add<ErrorFilter>();
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("Administrator",
-    authBuilder =>
-    {
-        authBuilder.RequireRole("Administrator");
-    });
-    options.AddPolicy("Manager",
-        authBuilder =>
-        {
-            authBuilder.RequireRole("Manager");
-        });
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddPresentation();
 }
 
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.Run();
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
